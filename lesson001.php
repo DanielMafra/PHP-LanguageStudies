76870806fa2c272fe98243ae3fbcee29107dd33c
Daniel Mafra <danielmafradev@gmail.com>
2021-07-30 11:28:45 -0300

    Add lesson001

---
+++ lesson001.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Basics</title>
+</head>
+<body>
+
+<?php
+
+// this is a one line comment
+/* this is a
+two lines comment */
+
+echo "Hello World!";
+print "My name is Daniel";
+
+?>
+  
+</body>
+</html>
