f230cc6ab3b606bf123b96c53d09f3cafbf03deb
Daniel Mafra <danielmafradev@gmail.com>
2021-07-30 12:31:48 -0300

    Add lesson005

---
+++ lesson005.php
@@ -0,0 +1,12 @@
+<?php
+//consts
+
+define("NAME", "Daniel Mafra");
+define("AGE", 24);
+define("HEIGHT", 1.71);
+define("COLORS", ['blue', 'yellow', 'green']);
+
+echo 'My name is '.NAME.', my age is '.AGE.' and my height is '.HEIGHT.'.';
+echo COLORS[0];
+
+?>
