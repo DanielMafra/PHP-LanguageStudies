39e525844957e0a59c9c6ac8a4f399ea445877a8
Daniel Mafra <danielmafradev@gmail.com>
2021-08-03 11:13:25 -0300

    Add lesson007

---
+++ lesson007.php
@@ -0,0 +1,18 @@
+<?php
+//if, elseif, else
+
+$number = 10;
+
+if ($number == 10):
+  echo "Is 10";
+elseif ($number == 9):
+  echo "Is 9";
+else:
+  echo "No is 10";
+endif;
+
+$average = 7;
+
+echo ($average >= 7) ? "Yes" : "No";
+
+?>
