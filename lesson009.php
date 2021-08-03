f76a6900d3560b8a369a9db6ad4bfdaed46ea883
Daniel Mafra <danielmafradev@gmail.com>
2021-08-03 11:23:16 -0300

    Add lesson009

---
+++ lesson009.php
@@ -0,0 +1,18 @@
+<?php
+//while & do while
+
+$counter = 1;
+
+while ($counter <= 10){
+  echo "Counter is $counter";
+  $counter++;
+}
+
+$counter = 1;
+
+do {
+  echo "Counter is $counter";
+  $counter++;
+} while($counter <= 10);
+
+?>
