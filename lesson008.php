156810bf5f0fc7059cd398e6c9c998c4774926cf
Daniel Mafra <danielmafradev@gmail.com>
2021-08-03 11:17:17 -0300

    Add lesson008

---
+++ lesson008.php
@@ -0,0 +1,18 @@
+<?php
+//switch case
+
+$color = "red";
+
+switch($color):
+  case "red":
+    echo "Is red";
+    break;
+  case "green":
+    echo "Is green";
+    break;
+  default:
+  echo "None";
+  break;
+endswitch;
+
+?>
